1d19b9864c1a97499c99f4ba9c99d3da4f627b17
Annstella <annstellawangui@gmail.com>
2018-08-10 15:44:44 +0300

    added date in src/app/quote/quote.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ export class QuoteComponent implements OnInit {
 
     quotes= [
       new Quote(1,'One of the happiest moments is having the courage to let go of something yo cannot change','Annstella Kagai')
-      new Quote(2,'Creativity takes courage','Henri Matisse')
+      new Quote(2,'Creativity takes courage','Henri Matisse'new date(1999/8/3))
     ]
   constructor() { }
 
