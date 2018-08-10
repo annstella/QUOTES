3c72a4a3e2956a5f4658c4b2519f0af08a7131a4
Annstella <annstellawangui@gmail.com>
2018-08-10 15:37:16 +0300

    added info in src/app/quote/quote.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+    quotes= [
+      new Quote(1,'One of the happiest moments is having the courage to let go of something yo cannot change','Annstella Kagai')
+      new Quote(2,'Creativity takes courage','Henri Matisse')
+    ]
   constructor() { }
 
   ngOnInit() {
