5496f3b32121841395714584356e80987150188e
Annstella <annstellawangui@gmail.com>
2018-08-12 22:59:26 +0300

    added info to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+      quotes = [
+        new Quote(1,'never give up','Stella',Date(12/4/1999),
+        new Quote(2,'Be strong','Barbie',Date(19/5/2000),
+
+      ]
 }
