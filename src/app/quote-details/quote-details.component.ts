f7d8d966cb844f2d51e597acef07b865cd0784f2
Annstella <annstellawangui@gmail.com>
2018-08-12 22:32:13 +0300

    added info in quote-details.component.ts

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import {Goal} from '../goal'
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
